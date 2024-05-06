e4d5871474d03bde5f7ffcb4d2967c14d71417e1
Abishek karki <140816422+abishekkarkiiii@users.noreply.github.com>
2024-05-06 18:55:33 +0545

    Add files via upload

---
+++ Ascending.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+
+int main() {
+    int arr[] = {4, 9, 5, 3, 2, 10};
+    int i, j, t;
+
+    for (i = 0; i < 6; i++) {
+        for (j = i + 1; j < 6; j++) {
+            if (arr[i] > arr[j]) {
+                t = arr[j];
+                arr[j] = arr[i];
+                arr[i] = t;
+            }
+        }
+    }
+
+    for (i = 0; i < 6; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
+
+
